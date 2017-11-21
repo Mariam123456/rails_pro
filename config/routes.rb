fff721be5197c11a18a93839c42bcf416ac1aed0
mariam123456 <aa.20071663@gmail.com>
2017-11-21 12:10:48 +0200

    3.10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'simple_pages#index'
   get 'simple_pages/product'
 
   get 'simple_pages/about'
@@ -7,6 +6,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
+  root 'simple_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
