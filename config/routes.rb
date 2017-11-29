8ca23bf794a88018adf941e6b06c6abf45f85b2e
mariam123456 <aa.20071663@gmail.com>
2017-11-29 10:52:29 +0200

    landingpage and order controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :products
   get 'simple_pages/product'
 
@@ -8,8 +9,10 @@ Rails.application.routes.draw do
 
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-
-  root 'simple_pages#index'
+  # root 'simple_pages#index'
+  root 'simple_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
+    get 'orders/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
