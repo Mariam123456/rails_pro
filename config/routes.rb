c84ea80a85893a63330bede54a00c83054302cf0
mariam123456 <aa.20071663@gmail.com>
2017-12-04 13:40:13 +0200

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   # root 'simple_pages#index'
-  root 'simple_pages#landing_page'
+  root 'simple_pages#about'
   resources :orders, only: [:index, :show, :create, :destroy]
-    get 'orders/create'
+  get 'orders/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
