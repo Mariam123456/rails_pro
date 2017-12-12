3ac5a9d348cea9f366cdcfb616d3ba3b9c91c605
mariam123456 <aa.20071663@gmail.com>
2017-12-12 14:35:58 +0200

    autheruzation 5.4

--- db/migrate/20171127081348_rename.rb
+++
@@ -1,5 +0,0 @@
-class Rename < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :products, :prices, :price
-  end
-end
--- db/migrate/20171211100522_add_price_to_products.rb
+++ db/migrate/20171211100522_add_price_to_products.rb
@@ -1,6 +1,5 @@
-class AddPriceToProduct < ActiveRecord::Migration[5.1]
+class AddPriceToProducts < ActiveRecord::Migration[5.1]
   def change
     add_column :products, :price, :float
-
   end
 end
