38471179c05df151849bf6f9d6e297ca6f817560
mariam123456 <aa.20071663@gmail.com>
2017-12-23 10:16:10 +0200

    Validation and PaginatioN

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
+
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :color, presence: true
+	validates :price, presence: true
+
 	def self.search(search_term)
 		like_type = Rails.env.production? ? "ilike" : "LIKE"
 		Product.where("name #{like_type} ?", "%#{search_term}%")
@@ -11,9 +17,9 @@ class Product < ApplicationRecord
 		comments.rating_desc.first.rating
 	end
 
-def average_rating
-  comments.average(:rating).to_f
-end
+	def average_rating
+		comments.average(:rating).to_f
+	end
 
 	def lowest_rating_comment
 		comments.rating_asc.first.rating
