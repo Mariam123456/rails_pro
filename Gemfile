098b3e5530d449112e3efb6beaeb6a030fafcd11
mariam123456 <aa.20071663@gmail.com>
2017-12-26 13:31:39 +0200

    rspec model testing 6.2

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'devise'
 gem 'cancancan', '~> 1.10'
 gem 'ratyrate'
 gem 'will_paginate', '~> 3.1.0'
+gem 'rails-controller-testing'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'sqlite3'
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe Product do 
 	context 'when the product has comments' do
-		let (:product) {Product.create!(price:"22",color:"red",description:"drink")}
-		let (:user) {User.create!(name: "mariam", last_name:"sak", email: "a@a.com", password: "123456")}
+		let (:product) {Product.create!(name:"mariam",price:"22",color:"red",description:"drink")}
+		let (:user) {User.create!(name: "mariam", email: "a@a.com", password: "123456")}
 
 
 		before do
@@ -23,6 +23,10 @@ describe Product do
 
 		it "is not valid without email" do
 			expect(User.new(name: "mariam")).not_to be_valid
+		end
+
+		it "is valid without last_name" do
+			expect(User.new(name: "mariam")).to be_valid
 		end
 
 	end
