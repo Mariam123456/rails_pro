57ba73c8d788258d41031ff734da11b4e32524fc
mariam123456 <aa.20071663@gmail.com>
2017-12-26 11:40:03 +0200

    rspec model test

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 
-	validates :name, presence: true
+	
 	validates :description, presence: true
 	validates :color, presence: true
 	validates :price, presence: true
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Product do 
 	context 'when the product has comments' do
-		let (:product) {Product.create!(name:"water",price:"22",color:"red",description:"drink")}
+		let (:product) {Product.create!(price:"22",color:"red",description:"drink")}
 		let (:user) {User.create!(name: "mariam", last_name:"sak", email: "a@a.com", password: "123456")}
 
 
@@ -19,6 +19,10 @@ describe Product do
 		
 		it "is not valid without a name" do
 			expect(Product.new(description: "drink")).not_to be_valid
+		end
+
+		it "is not valid without email" do
+			expect(User.new(name: "mariam")).not_to be_valid
 		end
 
 	end
