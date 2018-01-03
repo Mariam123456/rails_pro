c240bf83554ffb64971fd1e43eeff74508ab5d30
mariam123456 <aa.20071663@gmail.com>
2018-01-03 13:05:08 +0200

    6.4 using factories

---
+++ spec/factories/product_factory.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+name"mariam"
+price"22"
+color"red"
+description"drink"
+  end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,28 +2,26 @@ require 'rails_helper'
 
 describe Product do 
 	context 'when the product has comments' do
-		let (:product) {Product.create!(name:"mariam",price:"22",color:"red",description:"drink")}
-
-
+		
 		before do
+			@product = FactoryBot.create(:product)
 			@user = FactoryBot.create(:user)
-			product.comments.create!(rating: 1, user: @user, body: "bad!")
-			product.comments.create!(rating:3 , user: @user, body: "good!")
-			product.comments.create!(rating: 5, user: @user, body: "great!")
+			@product.comments.create!(rating: 1, user: @user, body: "bad!")
+			@product.comments.create!(rating:3 , user: @user, body: "good!")
+			@product.comments.create!(rating: 5, user: @user, body: "great!")
 			
 		end
 
 		it "returns the average rating of all comments" do
-			expect(product.average_rating).to eq(3)
+			expect(@product.average_rating).to eq(3)
 		end
 		
-		it "is not valid without a name" do
-			expect(Product.new(description: "drink")).not_to be_valid
-		end
 
 		it "is not valid without email" do
-			expect(User.new(name: "mariam")).not_to be_valid
+		  @user = FactoryBot.build(:user, email: "not_an_email")
+    		expect(@user).to_not be_valid
 		end
+
 	end
 end
 
