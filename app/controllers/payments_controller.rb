f263f1d7ad3b57d0b3323511e144908d612dca51
mariam123456 <aa.20071663@gmail.com>
2018-01-09 20:55:39 +0200

    payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,34 +1,37 @@
 class PaymentsController < ApplicationController
-	before_action :authenticate_user!
-	
-	def create
-		@product = Product.find(params[:product_id])
-		@user = current_user
-		token = params[:stripeToken]
-		begin
-		   charge = Stripe::Charge.create(
-                amount: (@product.price*100),
+
+    def create
+        @product = Product.find(params[:product_id])
+        @user = current_user
+
+        token = params[:stripeToken]
+         # Create the charge on Stripe's servers - this will charge the user's card
+        begin
+            charge = Stripe::Charge.create(
+                amount: (@product.price*100).to_i, # amount in cents, again
                 currency: "gbp",
                 source: token,
                 description: params[:stripeEmail]
-)
+            )
 
         if charge.paid
-          Order.create(
-            user_id: @user.id,
-            product_id: params[:product_id],
-            total: @product.price * 100,
-)
-            flash[:notice] = "Your payment was processed successfully #{@product.name}"
+            Order.create(
+              product_id: @product.id,
+              user_id: @user.id,
+               total: @product.price
+            )
+            flash[:success] = "Your payment was processed successfully"
         end
 
-		rescue Stripe::CardError => e
-			body = e.json_body
-			err = body[:error]
-			flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-		end
-		redirect_to product_path(@product)
-	end
+        rescue Stripe::CardError => e
+            body = e.json_body
+            err = body[:error]
+            flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+        end
+
+        redirect_to product_path(@product)
+
+    end
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104074336) do
+ActiveRecord::Schema.define(version: 20180109181343) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20180104074336) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
-    t.integer "price"
+    t.decimal "price"
   end
 
   create_table "rating_caches", force: :cascade do |t|
