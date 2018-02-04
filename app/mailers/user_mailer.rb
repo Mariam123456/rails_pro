c4756555d1562e8024a2d273a3a29f7507eae888
mariam123456 <aa.20071663@gmail.com>
2018-02-04 20:43:21 +0200

    send email in sign in

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,12 @@
 class UserMailer < ApplicationMailer
-    def contact_form(email, name, message)
-      mail(:to =>'mariamcareerf@gmail.com',
-           :subject=> "A new contact form message from #{name}",
-         :body => "You have new contact message from this email : #{email} with this message : #{message}")
-    end
+	def contact_form(email, name, message)
+		mail(:to =>'mariamcareerf@gmail.com',
+			:subject=> "A new contact form message from #{name}",
+			:body => "You have new contact message from this email : #{email} with this message : #{message}")
+	end
+	def welcome(user)
+		@appname = "food co market"
+		mail(to: user.email,
+			subject: "Welcome to #{@appname}!")
+	end
 end
